b1ddd4ab874e1770e244d26a871c0905e9f6a29d
aryan <aryanmundra666@gmail.com>
2021-06-14 23:40:39 +0530

    removed varaitions

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect,get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def _cart_id(request):
@@ -9,8 +10,21 @@ def _cart_id(request):
     if not cart:
         cart = request.session.create()
     return cart
+
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
+    # product_variation = []
+    # if request.method == 'POST':
+      # for item in request.POST:
+      #    key = item
+      #    value = request.POST[key]
+         #
+         # try:
+         #     variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+         #     product_variation.append(variation)
+         # except:
+         #     pass
+
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))#get the cart using the cart_id present in the session
     except Cart.DoesNotExist:
@@ -20,6 +34,9 @@ def add_cart(request, product_id):
     cart.save()
     try:
          cart_item = CartItem.objects.get(product=product, cart=cart)
+         # if len(product_variation)> 0:
+         #     for item in product_variation:
+         #         cart_item.variations.add(item)
          cart_item.quantity += 1
          cart_item.save()
     except CartItem.DoesNotExist:
@@ -28,7 +45,10 @@ def add_cart(request, product_id):
               quantity = 1,
               cart = cart,
          )
-    cart_item.save()
+         # if len(product_variation)> 0:
+         #      for item in product_variation:
+         #          cart_item.variations.add(item)
+         # cart_item.save()
     return redirect('cart')
 
 def remove_cart(request, product_id):
@@ -52,6 +72,8 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0 , quantity=0, cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart= Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -59,7 +81,7 @@ def cart(request, total=0 , quantity=0, cart_items=None):
             quantity +=cart_item.quantity
         tax =100
         grand_total = total +tax
-    except ObjectNotExists:
+    except ObjectDoesNotExist:
         pass
 
     context ={
