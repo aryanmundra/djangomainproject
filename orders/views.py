7ac3a2812b5f7ccac2a1b199745afd5602212766
aryan <aryanmundra666@gmail.com>
2021-07-19 22:27:23 +0530

    19 july

--- orders/views.py
+++ orders/views.py
@@ -7,14 +7,31 @@ from store.models import Product
 
 # Create your views here.
 def payments(request):
-    #move the cart items to order product table
+    def payments(request):
+     body = json.loads(request.body)
+     order = Order.objects.get(user=request.user, is_ordered=False)
+
+    # Store transaction details inside Payment model
+    payment = Payment(
+        user = request.user,
+        payment_method = body['payment_method'],
+        amount_paid = order.order_total,
+        status = body['status'],
+    )
+    payment.save()
+
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+
+    # Move the cart items to Order Product table
     cart_items = CartItem.objects.filter(user=request.user)
-    
+
 
     for item in cart_items:
         orderproduct = OrderProduct()
         orderproduct.order_id = order.id
-        # orderproduct.payment = payment
+        orderproduct.payment = payment
         orderproduct.user_id = request.user.id
         orderproduct.product_id = item.product_id
         orderproduct.quantity = item.quantity
@@ -22,8 +39,14 @@ def payments(request):
         orderproduct.ordered = True
         orderproduct.save()
 
+        cart_item = CartItem.objects.get(id=item.id)
+        product_variation = cart_item.variations.all()
+        orderproduct = OrderProduct.objects.get(id=orderproduct.id)
+        orderproduct.variations.set(product_variation)
+        orderproduct.save()
 
-    return render(request, 'orders/payments.html')
+
+    return JsonResponse(data)
 
 def place_order(request, total=0, quantity=0,):
     current_user = request.user
