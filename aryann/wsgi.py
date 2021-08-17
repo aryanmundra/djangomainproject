2d8d286bebb001ef2c4e5d1c25a8d4359496e2fb
aryanvit53 <aryanvit53@gmail.com>
2021-08-17 22:38:48 +0530

    q

--- aryann/wsgi.py
+++ aryann/wsgi.py
@@ -8,8 +8,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
-sys.path.append('/usr/local/lib/python2.7/site-packages')
+
 
 from django.core.wsgi import get_wsgi_application
 
