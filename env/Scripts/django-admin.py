e1306736bc1ebd3bb2e0ff8761cb95228f636f5d
aryanvit53 <aryanvit53@gmail.com>
2021-08-18 10:06:28 +0530

    django python

--- env/Scripts/django-admin.py
+++ env/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\aryan mahi\desktop\djangomainproject\env\scripts\python.exe
+#!C:\Users\Aryan Mahi\Desktop\djangomainproject\env\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
