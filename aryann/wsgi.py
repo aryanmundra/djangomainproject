bf22bddd28faaf2637526b93f3ddeac7f03934d2
aryanvit53 <aryanvit53@gmail.com>
2021-08-18 09:06:36 +0530

    a

--- aryann/wsgi.py
+++ aryann/wsgi.py
@@ -9,9 +9,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 
-import sys
 
-sys.path.append('/usr/local/lib/python3.6/site-packages')
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aryann.settings')
