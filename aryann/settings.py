e5fb7f9f211043cadc75a3fdf43e341f9517516d
aryanvit53 <aryanvit53@gmail.com>
2021-08-20 13:30:34 +0530

    s3

--- aryann/settings.py
+++ aryann/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'store',
     'carts',
     'orders',
-    # 'admin_honeypot',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -109,6 +109,16 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ebdb',
+#         'USER': 'aryanmundra',
+#         'PASSWORD': 'Aryan1705',
+#         'HOST': 'aa1uhact8j4ikd2.cpuunwon29l0.us-west-2.rds.amazonaws.com',
+#         'PORT': 5432,
+#     }
+# }
 
 
 
@@ -148,11 +158,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR /'static'
+# STATICFILES_DIRS = [
+# 'aryann/static',
+# ]
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
+
 STATICFILES_DIRS = [
-'aryann/static',
+    'aryann/static',
 ]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
