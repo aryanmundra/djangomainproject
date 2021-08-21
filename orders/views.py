ed27a94109c212109a68149944ca507e45115d37
aryanvit53 <aryanvit53@gmail.com>
2021-08-21 10:00:47 +0530

    50

--- orders/views.py
+++ orders/views.py
@@ -25,7 +25,7 @@ def place_order(request, total=0, quantity=0,):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-        tax = 100
+        tax = 50
         grand_total = total + tax
 
         if request.method == 'POST':
