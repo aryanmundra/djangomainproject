46da162443173a48e0788b680b5837807369fc1f
aryanvit53 <aryanvit53@gmail.com>
2021-08-25 10:03:44 +0530

    soites

--- aryann/settings.py
+++ aryann/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'category',
     'accounts',
     'store',
@@ -50,6 +51,8 @@ INSTALLED_APPS = [
     'orders',
     'storages',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
